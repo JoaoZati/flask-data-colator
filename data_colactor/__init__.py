fb21eddd4e04ad2aa77044cb8b19dad614fa97a2
JoaoZati <joaozati@gmail.com>
2021-10-06 12:40:50 -0300

    Taking the input from user

--- data_colactor/__init__.py
+++ data_colactor/__init__.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
 
+@app.route('/success', methods=['GET', 'POST'])
+def success():
+    if request.method == 'POST':
+        email = request.form['email_name']
+        height = request.form["height_name"]
+        print(email, height)
+        print(type(email), type(height))
+    return render_template('success.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    app.run()
