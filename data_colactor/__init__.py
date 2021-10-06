599b40438c83d9a28f9c65cf757d694e03a16222
JoaoZati <joaozati@gmail.com>
2021-10-06 15:20:03 -0300

    Criado a Database em postgresql - Usar o comando - pipenv install psycopg2-binary

--- data_colactor/__init__.py
+++ data_colactor/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://joao:af1235512355@localhost:5434/flask'
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = 'data'
+    id = db.Column(db.Integer, primary_key=True)
+    email_db = db.Column(db.String(150), unique=True)
+    height_db = db.Column(db.Integer)
+
+    def __init__(self, email_db, height_db):
+        self.email_db = email_db
+        self.height_db = height_db
 
 
 @app.route('/')
@@ -20,5 +34,6 @@ def success():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.debug = True
     app.run()
