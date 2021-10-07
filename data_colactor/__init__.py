6cfc4987462380372aa3ab0072fa1752f31c5c13
JoaoZati <joaozati@gmail.com>
2021-10-07 09:49:49 -0300

    Upload implemented - Program finalized

--- data_colactor/__init__.py
+++ data_colactor/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash, send_file
 from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
 from sqlalchemy.sql import func
@@ -29,20 +29,23 @@ def home():
 
 @app.route('/success', methods=['GET', 'POST'])
 def success():
+    global file
     if request.method == 'POST':
         file = request.files['file']
         if not file:
+            flash('Empty file', 'error')
             return redirect(url_for('home'))
         file.save(secure_filename(f'uploaded_{file.filename}'))
         with open(f'uploaded_{file.filename}', 'a') as f:
             f.write('This was add later')
+        flash('Upload successfully send', 'success')
         return render_template('home.html', btn=True)
     return redirect(url_for('home'))
 
 
 @app.route('/download')
 def download():
-    pass
+    return send_file(f'uploaded_{file.filename}', attachment_filename=f'yourfile_{file.filename}', as_attachment=True)
 
 
 if __name__ == '__main__':
