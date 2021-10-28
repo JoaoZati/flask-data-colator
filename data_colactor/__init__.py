f3d3129526b77b366ef4caf54078847b10cfd7eb
JoaoZati <joaozati@gmail.com>
2021-10-28 12:27:18 -0300

    - Test os.getenv

--- data_colactor/__init__.py
+++ data_colactor/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     db.init_app(app)
 
     from .views import views
